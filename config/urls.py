a97784a8d8909a5c4bb39b93f41f71dba4a9806a
Gabz047 <gabe1612mail@gmail.com>
2025-05-05 13:56:02 -0300

    FEAT: Created urls for views and registered models in admin

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from garagem.views import *
+
+router = DefaultRouter()
+
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"modelos", ModeloViewSet)
+router.register(r"veiculos", VeiculoViewSet)
+
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
