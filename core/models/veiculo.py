6e845e23539969e679c389bc928588f61f0e5eda
Gabz047 <gabe1612mail@gmail.com>
2025-05-06 21:51:50 -0300

    FIX: Consertado campo acessorio da model veiculos, trocando relação de 1 para muitos de muitos para muitos

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -8,7 +8,7 @@ class Veiculo(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name='veiculos')
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name='veiculos')
-    acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name='veiculos')
+    acessorio = models.ManyToManyField(Acessorio, blank=True)
 
     def __str__(self):
         return f"{self.id} - {self.modelo} - {self.cor} - {self.ano}" 
